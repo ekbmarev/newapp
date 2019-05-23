ca9d5ba2c19bb4f5995d335e2a7209a96e2d72ba
Ponomarev Alexandr <ekbmarev@gmail.com>
2019-05-23 15:51:00 +0300

    NewApps
    NewApps

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import connect from '@vkontakte/vkui-connect';
+import App from './app/App';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/index'
+// import registerServiceWorker from './sw';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducers, applyMiddleware(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Init VK App
+connect.send('VKWebAppInit', {});
+connect.send("VKWebAppGetGeodata", {});
+
+// Если вы хотите, чтобы ваше веб-приложение работало в оффлайне и загружалось быстрее,
+// расскомментируйте строку с registerServiceWorker();
+// Но не забывайте, что на данный момент у технологии есть достаточно подводных камней
+// Подробнее про сервис воркеры можно почитать тут — https://vk.cc/8MHpmT 
+// registerServiceWorker();
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <App />
+    </Provider>, 
+document.getElementById('root'));
